8b01589073ce7111a4d51f8192453e616a78ca4d
Progress Ochuko Eyaadah <devkoxy@gmail.com>
2024-07-11 19:54:21 +0100

    added balance info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
 import * as React from "react";
 import { toast } from "react-toastify";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import { useWallet } from "@solana/wallet-adapter-react";
+import { useWallet, useConnection } from "@solana/wallet-adapter-react";
+import * as web3 from "@solana/web3.js";
 
 const Home = () => {
   const wallet = useWallet();
+  const { connection } = useConnection();
 
+  const [balance, setBalance] = React.useState<number>(0);
+
+  const getInfo = async () => {
+    if (connection && wallet.publicKey) {
+      // we get the account info for the user's wallet data store and set the balance in our application's state
+      const info = await connection.getAccountInfo(wallet.publicKey);
+      setBalance(info!.lamports / web3.LAMPORTS_PER_SOL);
+    }
+  };
 
   return (
     <main className="min-h-screen  text-white">
@@ -14,31 +25,44 @@ const Home = () => {
           <div
             style={{
               borderWidth: "4px",
+              borderStyle: "solid",
               borderImage: "linear-gradient(to right, #4F46E5, #8F4AFE) 1",
               borderColor: "transparent",
+              borderRadius: "20%",
             }}
-            className="w-[300px]  sm:w-[600px] h-[400px] border-4"
+            className="w-[300px] rounded-md sm:w-[600px] h-[400px]"
           >
-            <div 
-               
-            >
+            <div>
               {wallet.connected ? (
-                <h1 className="text-center text-2xl pt-[50px]">
-                   Check your wallet for spare solana
-                </h1>
+                <div className="grid place-content-center">
+                  <button
+                    onClick={getInfo}
+                    style={{ opacity: balance > 0 ? 0.5 : 1 }}
+                    disabled={balance > 0}
+                    className="text-center border border-[#512da8] rounded-md px-3 py-2 text-2xl mt-[50px]"
+                  >
+                    Check your wallet
+                  </button>
+
+                  {balance ? (
+                    <p className="text-white mt-3 italic font-semibold">
+                      Your balance is {balance} sol
+                    </p>
+                  ) : null}
+
+
+                </div>
               ) : (
                 <h1 className="pt-[50px] text-center text-2xl">
-                  Connect to wallet
+                  Connect your wallet
                 </h1>
               )}
-             
             </div>
 
             <div className="grid place-content-center pt-[200px] ">
-              <button className="bg-[#512da8] rounded-sm px-[3px] py-[3px]">
-              <WalletMultiButton className="rounded-sm bg-[#512da8] " />
+              <button className="bg-[#512da8] rounded-md px-[3px] py-[3px]">
+                <WalletMultiButton className="rounded-sm bg-[#512da8] " />
               </button>
-              
             </div>
           </div>
         </div>
